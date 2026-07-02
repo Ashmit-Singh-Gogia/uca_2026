b0d30b1138e96c282fa0d825cc716ef0305a6f16
Ashmit Singh Gogia <ashmit9955@gmail.com>
2026-07-02 22:58:49 +0530

    created a structure for future assignments and classworks

---
+++ C/templates/_template.c
@@ -0,0 +1,11 @@
+/*
+ * Course   : C Programming (Mentor: )
+ * Type     : Class Work / Assignment #__
+ * Date     :
+ * Topic    :
+ * Problem  :
+ *   Write it in your own words the moment it's given -- especially
+ *   if the mentor only speaks it out loud. This comment is the only
+ *   copy of the problem statement that will exist, so don't skip it.
+ * Status   :
+ */
